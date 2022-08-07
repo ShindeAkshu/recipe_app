72c612aaeb0a8aed31e872299f19bb36c275be24
shindeAkshu <sakshata416@gmail.com>
2022-08-07 11:55:05 -0700

    Recipe - App

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@
 import './App.css';
 
 function App() {
+  const APP_ID = '1a2a1b1d';
+  const APP_KEY = 'd1de282e53eaa9a2bc9b495332514cb1';
+  const exampleReq =`https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}`;
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <form clasName="Search-form">
+        <input className = " search-bar" type="text"/>
+        <button className="search-button" type="Submit">Search</button>
+      </form>
     </div>
   );
 }
