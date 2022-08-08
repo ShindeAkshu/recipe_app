a2c9dfb3efb87dbad5425d5474ff93092a756164
shindeAkshu <sakshata416@gmail.com>
2022-08-08 10:42:13 -0700

    Recipe-App

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-
+import Recipe from './Recipe';
 import { useEffect } from 'react';
 import './App.css';
 
 function App() {
   const APP_ID = '1a2a1b1d';
   const APP_KEY = 'd1de282e53eaa9a2bc9b495332514cb1';
-  const exampleReq =`https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}`;
-  const[counter,setCounter] = useState (0);
-  useEffect(() => {
-    console.log('Effect has been run');
-  })
+  
+  const [recipes,setRecipes] = useState([])
+     useEffect(() => {
+     GetRecipes();
+  },[]);
+
+  const GetRecipes = async () => {
+    const response  = await featch (`https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}`)
+  
+    const data = await response.json();
+    setRecipes(data.hits);
+    console.log(data.hits);
+  };  
+   
+  
   return (
     <div className="App">
       <form clasName="Search-form">
         <input className = " search-bar" type="text"/>
         <button  className="search-button" type="Submit">Search</button>
       </form>
-      <h1 onClick={() => setCounter(counter + 1)}>{counter}</h1>
+      {recipes.map(recipe => (
+        <Recipe/>
+      ))}
     </div>
   );
 }
